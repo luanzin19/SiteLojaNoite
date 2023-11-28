d4756bc665388d072fa195cb96966b09c6344404
Luan Fonseca De Lima <128102163+luanzin19@users.noreply.github.com>
2023-11-28 19:36:51 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Segurança</title>
+</head>
+<body>
+	<center>
+		<h1>NOITE DA COMPUTARIA</h1>
+		<h3>Login</h3>
+		<form id="form-login" action="login.php" method="POST">
+			Login: <input type="text" name="login"><br>
+			Senha: <input type="password" name="senha"><br><br>
+			<input type="submit" name="entrar" value="Entrar">
+		</form>
+	</center>
+</body>
+</html>                               
